5942798903127c4f1242c5df50c756a169fa235c
iuliancarnaru <iulian.carnaru@gmail.com>
2020-06-04 18:07:22 +0100

    learning about chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,5 @@
 const validator = require('validator');
+const chalk = require('chalk');
 const add = require('./utils.js');
 const notes = require('./notes.js');
 
@@ -9,3 +10,9 @@ console.log(notes());
 
 console.log(validator.isEmail('test@example.com'));
 console.log(validator.isURL('http://www.example.com'));
+
+console.log(chalk.green.bold('Success'));
+console.log(chalk.red.bold('Error'));
+console.log(chalk.bgBlue.bold('Loading'));
+
+console.log(chalk`There are {yellow.bold 5280 feet} in a mile`);
