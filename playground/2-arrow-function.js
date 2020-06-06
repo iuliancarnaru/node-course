724f2bfbc9c261dba51e83383fa25a064b56c315
iuliancarnaru <iulian.carnaru@gmail.com>
2020-06-06 13:31:56 +0100

    refreshing this on arrow functions

---
+++ playground/2-arrow-function.js
@@ -0,0 +1,43 @@
+const square = function (x) {
+  return x * x;
+};
+
+const squareArrow = (x) => {
+  return x * x;
+};
+
+const squareArrowTwo = (x) => x * x;
+
+const event = {
+  name: "Birthday Party",
+  printGuestList: function () {
+    console.log(`Guest list for ${this.name}`);
+  },
+};
+
+const eventTwo = {
+  name: "Birthday Party",
+  printGuestList: () => {
+    // arrow functions don't bind their own this value (name is undefined)
+    console.log(`Guest list for ${this.name}`);
+  },
+};
+
+const eventThree = {
+  name: "Birthday Party",
+  // method printGuestList on object eventThree
+  guestList: ["Iulian", "Paul", "Aneta"],
+  printGuestList() {
+    console.log(`Guest list for ${this.name}`);
+    this.guestList.forEach((guest) => {
+      // this binding of the arrow function is to the printGuestList
+      // and is having access to the this of the object
+      console.log(`${guest} is attending ${this.name}`);
+    });
+  },
+};
+
+console.log(square(3));
+console.log(squareArrow(3));
+console.log(squareArrowTwo(3));
+console.log(event.printGuestList());
