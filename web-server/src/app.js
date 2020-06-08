aa387fd4498865c3afe00d5bd2180e479073649f
iuliancarnaru <iulian.carnaru@gmail.com>
2020-06-08 17:31:54 +0100

    setting up pug as a template engine for our server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,6 +4,26 @@ const express = require("express");
 const app = express();
 const publicDirectory = path.join(__dirname, "../public");
 app.use(express.static(publicDirectory));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("homepage", {
+    title: "Weather App",
+    name: "Iulian Carnaru",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About page",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help page",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
