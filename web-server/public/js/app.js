5f56352cf5d0aa3bcc9ee0d19ceeaee50c5d6a4c
iuliancarnaru <iulian.carnaru@gmail.com>
2020-06-12 10:56:38 +0100

    fetching data on client side

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,6 @@
 console.log("Client side JS is loaded");
+
+fetch(`http://localhost:3000/weather?address="bucharest"`)
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
