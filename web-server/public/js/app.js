353ca3dd9745951deeb302154b3807fc376416ba
iuliancarnaru <iulian.carnaru@gmail.com>
2020-06-12 11:16:45 +0100

    connecting client side to backend

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,6 +1,13 @@
 console.log("Client side JS is loaded");
 
-fetch(`http://localhost:3000/weather?address="bucharest"`)
-  .then((res) => res.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error));
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const location = search.value;
+  fetch(`http://localhost:3000/weather?address=${location}`)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+});
