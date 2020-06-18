7b95a7dc81714caa4d689ce3e11fd91fb57eb389
Iulian Carnaru <iulian.carnaru@gmail.com>
2020-06-18 16:59:07 +0100

    updating schemas

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -2,11 +2,26 @@ const express = require("express");
 require("./db/mongoose");
 
 const User = require("./models/User");
+const Task = require("./models/Task");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  console.log(req.params);
+});
 
 app.post("/users", (req, res) => {
   const user = new User(req.body);
@@ -16,7 +31,18 @@ app.post("/users", (req, res) => {
     .then(() => {
       res.send(user);
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => res.send(task))
+    .catch((err) => res.status(400).send(err));
 });
 
 app.listen(port, () => {
