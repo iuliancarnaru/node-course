f4580808bd770e13b6b9b10ed21ff2e7d4770db7
Iulian Carnaru <iulian.carnaru@gmail.com>
2020-06-25 17:32:44 +0100

    working with files, avatar images

--- task-app/src/routers/user.js
+++ task-app/src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer");
 const User = require("../models/User");
 const auth = require("../middleware/auth");
 
@@ -92,6 +93,24 @@ router.delete("/users/me", auth, async (req, res) => {
   } catch (error) {
     res.status(500).send();
   }
+});
+
+const upload = multer({
+  dest: "avatars",
+  limits: {
+    filesize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please upload a image file"));
+    }
+
+    cb(undefined, true);
+  },
+});
+
+router.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
+  res.send();
 });
 
 module.exports = router;
