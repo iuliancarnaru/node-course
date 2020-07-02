482e8e918b5c74b471e9eb5a351f61be05c06326
Iulian Carnaru <iulian.carnaru@gmail.com>
2020-07-02 16:40:47 +0100

    more task tests

--- task-app/src/tests/fixtures/db.js
+++ task-app/src/tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const User = require("../../models/User");
+const Task = require("../../models/Task");
 
 const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
@@ -15,13 +16,62 @@ const userOne = {
   ],
 };
 
+const userTwoId = new mongoose.Types.ObjectId();
+const userTwo = {
+  _id: userTwoId,
+  name: "Jane Doe",
+  email: "jane@example.com",
+  password: "jane1234",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET),
+    },
+  ],
+};
+
+const taskOne = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "Finish Node tutorial",
+  completed: true,
+  createdBy: userOneId,
+};
+
+const taskTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "Start Typescript tutorial",
+  completed: false,
+  createdBy: userOneId,
+};
+
+const taskThree = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "Finish Deno tutorial",
+  completed: false,
+  createdBy: userTwoId,
+};
+
 const setupDatabase = async () => {
+  // cleaning existing database
   await User.deleteMany();
+  await Task.deleteMany();
+
+  // create and save the users
   await new User(userOne).save();
+  await new User(userTwo).save();
+
+  // create and save the tasks
+  await new Task(taskOne).save();
+  await new Task(taskTwo).save();
+  await new Task(taskThree).save();
 };
 
 module.exports = {
   userOneId,
   userOne,
+  userTwoId,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree,
   setupDatabase,
 };
--- task-app/src/tests/task.test.js
+++ task-app/src/tests/task.test.js
@@ -1,4 +1,50 @@
 const request = require("supertest");
-const Task = require("../models/Task");
+require("dotenv").config();
 
-test("Should create task for user", async () => {});
+const app = require("../app");
+const Task = require("../models/Task");
+const {
+  userOneId,
+  userOne,
+  userTwoId,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree,
+  setupDatabase,
+} = require("./fixtures/db");
+
+beforeEach(setupDatabase);
+
+test("Should create task for user", async () => {
+  const response = await request(app)
+    .post("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({ description: "From my test env" })
+    .expect(201);
+
+  const task = await Task.findById(response.body._id);
+  expect(task).not.toBeNull();
+  expect(task.completed).toEqual(false);
+});
+
+test("Should fetch user tasks", async () => {
+  const response = await request(app)
+    .get("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.length).toEqual(2);
+});
+
+test("Should not delete other user tasks", async () => {
+  await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404);
+
+  const taskOne = await TaskOne.findById(taskOne._id);
+  expect(task).not.toBeNull();
+});
