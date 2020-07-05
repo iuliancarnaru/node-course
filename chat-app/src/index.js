6f4334e81fc34a6f1e511c25c63c6e3bb8cee3b9
Iulian Carnaru <iulian.carnaru@gmail.com>
2020-07-05 21:18:26 +0100

    adding socketio

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.listen(port, () => {
+io.on("connection", () => {
+  console.log("new socket connection");
+});
+
+server.listen(port, () => {
   console.log(`App up and running at http://localhost:${port}`);
 });
