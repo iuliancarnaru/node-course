4f40ad1995edb4acf64d423aaa94c5b9da23f0d8
Iulian Carnaru <iulian.carnaru@gmail.com>
2020-07-06 16:56:50 +0100

    learning about sockets

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -11,8 +11,17 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-io.on("connection", () => {
+let count = 0;
+
+io.on("connection", (socket) => {
   console.log("new socket connection");
+  socket.emit("countUpdated", count);
+
+  socket.on("increment", () => {
+    count++;
+    // socket.emit("countUpdated", count);
+    io.emit("countUpdated", count);
+  });
 });
 
 server.listen(port, () => {
