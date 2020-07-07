4f6899558ae1d4e63fbe0bf57d1ac611c7ae89d5
Iulian Carnaru <iulian.carnaru@gmail.com>
2020-07-07 13:32:26 +0100

    disabling buttons when data is fetching

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,23 +1,28 @@
 const socket = io();
 
-// socket.on("countUpdated", (count) => {
-//   console.log("The count has been updated", count);
-// });
-
-// document.querySelector("#increment").addEventListener("click", () => {
-//   console.log("Clicked");
-//   socket.emit("increment");
-// });
+// elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
 
 socket.on("message", (message) => {
   console.log(message);
 });
 
-document.querySelector("#message-form").addEventListener("submit", (event) => {
+$messageForm.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  // disable form after message is submitted
+  $messageFormButton.setAttribute("disabled", "disabled");
+
   const message = event.target.elements.message.value;
 
   socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
     if (error) {
       return console.log(error);
     }
@@ -26,14 +31,17 @@ document.querySelector("#message-form").addEventListener("submit", (event) => {
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", (event) => {
+$sendLocationButton.addEventListener("click", (event) => {
   if (!navigator.geolocation) {
     return alert("Geolocation not suported by your browser");
   }
 
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
   navigator.geolocation.getCurrentPosition((position) => {
     const { latitude, longitude } = position.coords;
     socket.emit("sendLocation", { latitude, longitude }, () => {
+      $sendLocationButton.removeAttribute("disabled");
       console.log("Location shared");
     });
   });
